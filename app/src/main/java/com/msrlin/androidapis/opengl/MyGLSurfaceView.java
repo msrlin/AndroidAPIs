9d43ae9fdc43455861d7d29adcd0f25b7eb7929b
msrlin <xlinxy@163.com>
2016-01-22 18:14:40 +0800

    2016-1-5 17:35:11 修改包名，新增OpengleActivity

--- app/src/main/java/com/msrlin/androidapis/opengl/MyGLSurfaceView.java
+++ app/src/main/java/com/msrlin/androidapis/opengl/MyGLSurfaceView.java
@@ -3,22 +3,20 @@ package com.msrlin.androidapis.opengl;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Created by msrlin on 2016/1/22.
  */
 public class MyGLSurfaceView  extends GLSurfaceView{
-    private final MyGLRenderer mRenderer;
+    private final MyGLRenderer myGLRenderer;
 
     public MyGLSurfaceView(Context context){
                 super(context);
             setEGLContextClientVersion(2);  // Create an OpenGL ES 2.0 context
-
+        myGLRenderer= new MyGLRenderer();
+        setRenderer(myGLRenderer);
         }
     }
 
 
 
-}
